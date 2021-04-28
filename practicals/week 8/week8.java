97925ce402033d300c17fd916dc6fc2be90264ff
npapera <ngere.papera@students.mq.edu.au>
2021-04-28 15:48:11 +1000

    week8 prac

---
+++ practicals/week 8/week8.java
@@ -0,0 +1,154 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class week8 {
+
+	public static void main(String[] args) throws Exception{
+		try {
+			Socket sock = new Socket("localhost", 50000);
+			System.out.println("# java Client updated 21-April, 2021 @ MQ");
+			System.out.print("# Client-side simulator started with java MyClient");
+			String rcvd = "";
+			String capableServer = "";
+			String[] job;
+			String[] data;
+			String[] cap;
+			int counter = 0;
+			boolean isNewline = false;
+			boolean dataCheck = true;
+			
+			//checks for command line arguments
+			for(int i = 0; i < args.length; i++){
+				if(args[i].equalsIgnoreCase("-n")) {
+					isNewline = true;
+				}
+				//displays command line arguments after "java MyClient"
+				if(!args[i].equals("")){
+					if(i <= (args.length-1)){
+						System.out.print(" " + args[i]);
+					}
+				}
+			}
+			System.out.println("");
+
+			//handshake messages between client and server
+			send("HELO", sock, isNewline);
+			rcvd = receive(sock, isNewline);
+			if(rcvd.equals("OK")){
+			send("AUTH "+System.getProperty("user.name"), sock, isNewline); //authenticates username
+			} else {
+				error(sock, isNewline);
+			}
+
+			//read ds-system.xml
+			SystemInfo si = new SystemInfo();
+			int fileread = si.readXML("ds-system.xml");
+
+			//check if file has been read successfully
+			rcvd = receive(sock, isNewline);
+			if(fileread == 0){
+				if(rcvd.equals("OK")){
+					send("REDY", sock, isNewline);
+				} else {
+					error(sock, isNewline);
+				}
+			} else {
+				error(sock, isNewline);
+			}
+
+			//job scheduler
+			rcvd = receive(sock, isNewline);
+			while(!rcvd.contains("NONE")){
+				if(rcvd.length() > 0)
+					if(rcvd.contains("JOBN")){
+						job = rcvd.split(" ");
+						send("GETS Capable " + job[4] + " " + job[5] + " " + job[6], sock, isNewline);
+					} else if(rcvd.equals(".")){
+						send("SCHD " + counter + " " + capableServer + " 0", sock, isNewline);
+						counter++;
+					} else if(rcvd.contains("DATA")){
+						send("OK", sock, isNewline);
+						dataCheck = true;
+					} else if(rcvd.equals("OK")||rcvd.contains("JCPL")){
+						send("REDY", sock, isNewline);
+					} else if(rcvd.contains("ERR")){
+						error(sock, isNewline);
+					} else {
+						if(dataCheck){
+							data = rcvd.split("\n");
+							cap = data[data.length-1].split(" ");
+							System.out.println(Arrays.toString(cap));
+							capableServer = cap[0];
+							dataCheck = false;
+						}
+						send("OK", sock, isNewline);
+				}
+				rcvd = receive(sock, isNewline);
+			}
+
+			//quit protocol
+			if(rcvd.contains("NONE")) {
+                send("QUIT", sock, isNewline);
+                rcvd = receive(sock, isNewline);
+                if(rcvd.contains("QUIT")) {
+                    sock.close();
+		    		System.exit(0);
+                }
+            } else {
+                error(sock, isNewline);
+            }
+		
+		} catch(Exception e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	//send messages while checking for newline (if needed) and flushes the stream
+	private static void send(String str, Socket sOut, boolean newline) throws IOException{
+		DataOutputStream dataOut = new DataOutputStream(sOut.getOutputStream());
+		if(newline){
+			dataOut.write((str+"\n").getBytes());
+		} else {
+			dataOut.write(str.getBytes());
+		}
+		dataOut.flush();
+	}
+
+	//receives messages while checking for newline (if needed) and returns a string
+	private static String receive(Socket sIn, boolean newline) throws IOException{
+		DataInputStream dataIn = new DataInputStream(sIn.getInputStream());
+		String temp = "";
+		char cha = '\0';
+		while(dataIn.available()!=0){
+			cha = (char)dataIn.readByte();
+			temp += cha;
+		}
+		if(newline){
+			temp = temp.replace("\n", "");
+		}
+		return temp;
+	}
+
+	//checks the list of servers and determines the largest server by core count
+	private static String largestServer(List<ServerInfo> servers){
+		String bigServer = ""; //name of biggest server
+	        Integer largest = 0;
+		for (ServerInfo server : servers) {
+	        if(server.serverCoreCount > largest) { //loop to find the first largest core count 
+                largest = server.serverCoreCount; 
+                bigServer = server.serverType;
+            }
+        }
+        return bigServer;
+    }
+
+	//sends an error and exits the client safely
+	private static void error(Socket soc, boolean newline) throws IOException {
+        send("QUIT", soc, newline);
+        soc.close();
+		System.exit(1);
+    }
+
+}
